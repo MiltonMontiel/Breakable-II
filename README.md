f9b62d5189a371b7c4137c6539f9711c46989f65
Milton Gonzalez <milton.gonzalez@ENCEDCCDMLT0498.local>
2025-02-20 13:14:13 -0600

    chore: Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Breakable Toy II - Spotify API. 
+
+## Functional requirements. 
+This is a full-stack application that integrates with the Spotify API to fetch and display information about artists, albums, and tracks.
+
+
---
+++ back/src/test/java/com/breakabletoy/breakabletoy/BreakabletoyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breakabletoy.breakabletoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakabletoyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
